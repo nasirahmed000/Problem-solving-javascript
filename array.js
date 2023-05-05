f33df131b82a35db6386a40e4ac35ef0146a00b0
Nasir Ahmed <ahsannasirahmed@gmail.com>
2023-05-05 23:59:59 +0600

    3

--- array.js
+++ array.js
@@ -1,2 +1,23 @@
-const nasir = [ 1.2 ,3.5];
-console.log(nasir.length);
+const nasir = [ 1.2 ,3.5 ,5,5,5,5,6,8,4,6,4,74,54,64,64,64];
+
+console.log(Array.isArray(nasir));
+
+
+typeof operand
+console.log(typeof 42); // "number"
+console.log(typeof "Hello, world!"); // "string"
+console.log(typeof true); // "boolean"
+console.log(typeof undefined); // "undefined"
+console.log(typeof null); // "object" (Note: this is a known bug in JavaScript)
+console.log(typeof [1, 2, 3]); // "object"
+console.log(typeof { name: "John", age: 30 }); // "object"
+console.log(typeof function () {}); // "function"
+
+
+// concat 
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const combined = arr1.concat(arr2);
+
+console.log(combined); // [1, 2, 3, 4, 5, 6]
---
+++ slice.js
@@ -0,0 +1,10 @@
+const friends = [24,15,45,87,98,36,12,25,87,55,36];
+const result = friends.slice(2,5);
+console.log(result);
+
+
+const friendss = [24,15,45,87,98,36,12,25,87,55,36];
+const results = friends.splice(2,5);
+console.log(results);
+
+
