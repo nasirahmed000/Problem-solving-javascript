7c9d83e03af796bad28fcc09500d8e831ac1c814
Nasir Ahmed <ahsannasirahmed@gmail.com>
2023-05-06 17:26:16 +0600

    3

---
+++ duplicate.js
@@ -0,0 +1,35 @@
+const names = ['Baul','kaul','caul','daul','maul', 'Baul ', 'caul']
+
+function removeDuplicate(names){
+    const unique = [];
+    for(let i = 0 ; i= names.length ;i++){
+        const name= [i]
+        if( unique.includes (name)===false){
+            unique.push(name);
+        }
+        
+   
+}
+return unique;
+}
+const uniqueNames= removeDuplicate(names);
+console.log(uniqueNames);
+
+
+
+
+
+
+
+
+
+
+
+let arr = [1, 2, 3, 2, 4, 3];
+
+let uniqueArr = arr.filter((item, index) => {
+  return arr.indexOf(item) === index;
+});
+
+console.log(uniqueArr); 
+// Output: [1, 2, 3, 4]
