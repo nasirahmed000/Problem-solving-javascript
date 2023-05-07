e5d45e64ed85fe727c8f1ede934a90acc240035f
Nasir Ahmed <ahsannasirahmed@gmail.com>
2023-05-07 14:15:56 +0600

    j

---
+++ object.js
@@ -0,0 +1,15 @@
+const person = {
+    name: 'John',
+    age: 30,
+    isMarried: false,
+    hobbies: ['reading', 'swimming', 'gaming'],
+    address: {
+      street: '123 Main St',
+      city: 'New York',
+      state: 'NY'
+    },
+    sayHello: function() {
+      console.log(`Hello, my name is ${this.name}.`);
+    }
+  };
+  
