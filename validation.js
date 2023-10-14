ce610d3245c87b80b1fc6695f968b39aa3da3697
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2023-10-14 23:09:09 +0600

    Update validation.js

--- validation.js
+++ validation.js
@@ -10,4 +10,18 @@ function calculateArea(width, height) {
   let area = calculateArea(10, '5');
   
   console.log(area); // This line will not be executed because an error is thrown
+
+
+function calculateArea(width, height) {
+    if (typeof width !== 'number' || typeof height !== 'number') {
+      throw new Error('Both width and height must be numbers.');
+    }
+  
+    return width * height;
+  }
+  
+
+  let area = calculateArea(10, '5');
+  
+  console.log(area); // This line will not be executed because an error is thrown
   
