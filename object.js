129720bd2b583f6e40b56cbcfbca7a915500dacb
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2023-10-14 23:08:12 +0600

    Update object.js

--- object.js
+++ object.js
@@ -12,4 +12,22 @@ const person = {
       console.log(`Hello, my name is ${this.name}.`);
     }
   };
+
+
+
+
+const person = {
+    name: 'John',
+    age: 30,
+    isMarried: false,
+    hobbies: ['reading', 'swimming', 'gaming'],
+    address: {
+      street: '123 Main St',
+      city: 'New York',
+      state: 'NY'
+    },
+    sayHello: function() {
+      console.log(`Hello, my name is ${this.name}.`);
+    }
+  };
   
