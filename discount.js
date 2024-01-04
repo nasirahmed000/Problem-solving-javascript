8e30232a4b541a640ba7ce61be8caa95c791c2c5
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-01-05 01:43:41 +0600

    Update discount.js

--- discount.js
+++ discount.js
@@ -1,3 +1,23 @@
+// function declaration 
+function add (first ,second){
+    const total = first + second ;
+    return total;
+}
+// function expresssion  
+const add2 = function (first ,second){
+    const total = first+second ;
+    return total;
+}
+// arrow function 
+const add3 = (first, second) => first+second;
+
+const result =  add3 (10, 20);
+console.log(result);
+
+
+
+
+
 function calculateDiscountPrice(quantity, price) {
     let discountPrice = 0;
     
