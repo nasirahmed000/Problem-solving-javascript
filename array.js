0ed647e211acfdb01f5288a8534f6c51bfe4155e
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-01-05 02:01:04 +0600

    Update array.js

--- array.js
+++ array.js
@@ -1,3 +1,24 @@
+const add = (first , second ) => first + second ;
+const getFullName = (first , second) => first + "" + second;
+const multiply = (a,b) => a*b;
+
+const result = multiply (5 ,8);
+console.log(result);
+
+
+const multiple = (a,b , c,d, e, f) => a+b+c+d+e+f ;
+
+const onePie = () => 3.14;
+
+const number = (num) => num * 6 ;
+
+const number2 = num => num * 8 ; 
+
+
+
+
+
+
 const nasir = [ 1.2 ,3.5 ,5,5,5,5,6,8,4,6,4,74,54,64,64,64];
 
 console.log(Array.isArray(nasir));
