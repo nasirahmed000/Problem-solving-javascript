7f2a151ebbd7d3d8f691c910686e9958e41bdcfa
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-02-27 01:17:36 +0600

    Create ternary-operator.js

---
+++ ternary-operator.js
@@ -0,0 +1,9 @@
+// In JavaScript, the ternary operator is a concise way to write a conditional (if-else) statement. It is also known as the conditional operator. The syntax of the ternary operator is as follows:
+
+---> condition ? expressionIfTrue : expressionIfFalse;
+
+var isSunny = true;
+
+var weather = isSunny ? "It's a sunny day!" : "It's not sunny today.";
+
+console.log(weather);
