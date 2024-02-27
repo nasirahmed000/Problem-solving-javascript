949bb9670158254210607992166758d7ecff179b
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-02-28 03:39:16 +0600

    Create hoisting.js

---
+++ hoisting.js
@@ -0,0 +1,23 @@
+// eg 1
+
+
+<p id="demo"></p>
+
+<script>
+x = 5; // Assign 5 to x
+
+elem = document.getElementById("demo"); // Find an element 
+elem.innerHTML = x;           // Display x in the element
+
+var x; // Declare x
+</script>
+
+// eg 2 
+  
+<script>
+var x; // Declare x
+x = 5; // Assign 5 to x
+
+elem = document.getElementById("demo"); // Find an element 
+elem.innerHTML = x;           // Display x in the element
+</script>
