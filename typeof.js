8533a03271f54078f54a13b359333fd7ebb42269
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-03-08 23:00:55 +0600

    Create typeof.js

---
+++ typeof.js
@@ -0,0 +1,27 @@
+let num = 10;
+console.log(typeof num); // Output: "number"
+
+let str = "Hello";
+console.log(typeof str); // Output: "string"
+
+let bool = true;
+console.log(typeof bool); // Output: "boolean"
+
+let obj = {};
+console.log(typeof obj); // Output: "object"
+
+let func = function() {};
+console.log(typeof func); // Output: "function"
+
+
+
+"'John' is " + typeof "John" + "<br>" +
+"3.14 is " + typeof 3.14 + "<br>" +
+"NaN is " + typeof NaN + "<br>" +
+"false is " + typeof false + "<br>" +
+"[1, 2, 3, 4] is " + typeof [1, 2, 3, 4] + "<br>" +
+"{name:'John', age:34} is " + typeof {name:'John', age:34} + "<br>" +
+"new Date() is " + typeof new Date() + "<br>" +
+"function () {} is " + typeof function () {} + "<br>" +
+"myCar is " + typeof myCar + "<br>" +
+"null is " + typeof null;
