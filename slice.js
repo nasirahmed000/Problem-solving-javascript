87c0838ddd76be33b83de1469563ceb01ba1ffd5
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-03-16 06:37:33 +0600

    Update slice.js

--- slice.js
+++ slice.js
@@ -1,3 +1,31 @@
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+console.log(animals.slice(2));
+// Expected output: Array ["camel", "duck", "elephant"]
+
+console.log(animals.slice(2, 4));
+// Expected output: Array ["camel", "duck"]
+
+
+console.log(animals.slice(1, 5));
+// Expected output: Array ["bison", "camel", "duck", "elephant"]
+
+
+console.log(animals.slice(-2));
+// Expected output: Array ["duck", "elephant"]
+
+
+console.log(animals.slice(2, -1));
+// Expected output: Array ["camel", "duck"]
+
+
+console.log(animals.slice());
+// Expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
+
+
+
+
+
 const friends = [24,15,45,87,98,36,12,25,87,55,36];
 const result = friends.slice(2,5);
 console.log(result);
