58ae49dc0e75e6b34ba148d8b1c5d2bdc4cb75f2
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-04-21 22:23:57 +0600

    Create context-api.js

---
+++ context-api.js
@@ -0,0 +1,22 @@
+const ThemeContext = React.createContext('light');
+
+function App() {
+  return (
+    <ThemeContext.Provider value="dark">
+      <Toolbar />
+    </ThemeContext.Provider>
+  );
+}
+
+function Toolbar() {
+  return (
+    <div>
+      <ThemedButton />
+    </div>
+  );
+}
+
+function ThemedButton() {
+  const theme = React.useContext(ThemeContext);
+  return <button style={{ background: theme === 'dark' ? '#333' : '#FFF' }}>Themed Button</button>;
+}
