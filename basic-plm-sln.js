6e90a53f7473f14a23007d98e435e7c3c274ffb4
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-06-15 01:02:53 +0600

    Update basic-plm-sln.js

--- basic-plm-sln.js
+++ basic-plm-sln.js
@@ -69,7 +69,47 @@ function isPrime(num) {
 
 console.log(isPrime(7)); // Output: true
 
-6. Array
+// 6. reverse  
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Example usage:
+console.log(reverseString("hello")); // Output: "olleh"
+
+// 7. find largest number in Array 
+
+
+function findLargestNumber(arr) {
+    return Math.max(...arr);
+}
+
+// Example usage:
+console.log(findLargestNumber([1, 3, 5, 7, 9])); // Output: 9
+
+
+
+// 8.Write a function that prints the numbers from 1 to 100. But for multiples of three, print "Fizz" instead of the number, and for the multiples of five, print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz".
+
+
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+// Example usage:
+fizzBuzz();
+
 
 
 // //regards 
