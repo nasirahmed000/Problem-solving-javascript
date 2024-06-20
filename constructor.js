1d5292e901a62148a668300e939460fe8236a520
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-06-20 11:52:17 +0600

    Create constructor.js

---
+++ constructor.js
@@ -0,0 +1,60 @@
+// Here’s an example demonstrating inheritance using ES6 classes:
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        console.log(`${this.name} makes a noise.`);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, breed) {
+        super(name); // Call the parent class constructor
+        this.breed = breed;
+    }
+
+    speak() {
+        console.log(`${this.name} barks.`);
+    }
+}
+
+const dog = new Dog('Rex', 'German Shepherd');
+dog.speak(); // Output: Rex barks.
+
+
+
+// ***********************************************************************************
+// And using function constructors:
+
+function Animal(name) {
+    this.name = name;
+}
+
+Animal.prototype.speak = function() {
+    console.log(`${this.name} makes a noise.`);
+};
+
+function Dog(name, breed) {
+    Animal.call(this, name); // Call the parent constructor
+    this.breed = breed;
+}
+
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.speak = function() {
+    console.log(`${this.name} barks.`);
+};
+
+const dog = new Dog('Rex', 'German Shepherd');
+dog.speak(); // Output: Rex barks.
+
+
+
+
+
+
+
