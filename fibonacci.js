e244f76239a6ee457298e7bf7def94bd48f682bd
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-07-02 11:28:07 +0600

    Create fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,16 @@
+ // fibonacci series problem solution 
+
+
+
+const fibo = [0,1];
+for (let i=2;i<=20;i++){
+    fibo [i]= fibo [i-1]+fibo [i-2];
+}
+console.log(fibo);
+
+// output:  [
+//      0,    1,    1,   2,   3,    5,
+//      8,   13,   21,  34,  55,   89,
+//    144,  233,  377, 610, 987, 1597,
+//   2584, 4181, 6765
+// ]
