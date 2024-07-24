a99caeeb4761ed6a34ce8c366e7c18fc89e6c689
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-07-24 21:29:08 +0600

    Create Merge Two Sorted Arrays.js

---
+++ Merge Two Sorted Arrays.js
@@ -0,0 +1,34 @@
+// Problem:
+// Write a function to merge two sorted arrays into a single sorted array.
+
+sln:
+
+function mergeSortedArrays(arr1, arr2) {
+    let mergedArray = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergedArray.push(arr1[i]);
+            i++;
+        } else {
+            mergedArray.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        mergedArray.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        mergedArray.push(arr2[j]);
+        j++;
+    }
+
+    return mergedArray;
+}
+
+console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
