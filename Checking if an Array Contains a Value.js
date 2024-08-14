30495427479556528162ec0557956d6776923ecf
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2024-08-14 14:19:12 +0600

    Create  Checking if an Array Contains a Value.js

---
+++ Checking if an Array Contains a Value.js
@@ -0,0 +1,7 @@
+// To check if an array contains a specific value, use the includes method: 
+
+const fruits = ['apple', 'banana', 'mango'];
+const hasBanana = fruits.includes('banana');
+console.log(hasBanana); 
+
+// Output: true
