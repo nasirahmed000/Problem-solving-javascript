5f026e57ec29ce9caa7d072004b0deead7ac0bba
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2025-01-16 14:20:47 +0600

    Update Assync&FunctionalCoding.js

--- Assync&FunctionalCoding.js
+++ Assync&FunctionalCoding.js
@@ -44,16 +44,17 @@ promise.then((message) => {
 // Better User Experience: Ensures a smooth and responsive user experience.
 
 
+// normal function vs Arrow function 
+
+const add = function (first,second){
+    return first+second;
+}
+const result = add (10,20);
+console.log(result);
 
 
 
+const add =(first,second)=>first+second;
+const result = add(10,20);
+console.log(result)
 
-
-
-
-
-
-
-
-    
-});
