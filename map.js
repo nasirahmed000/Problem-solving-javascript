993003df6e8bb97180a1083e2ad4caf83ce061ce
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2025-08-30 00:25:43 +0600

    Update map.js

--- map.js
+++ map.js
@@ -1,3 +1,20 @@
 const fiveTimes = [1,2,3,4,5,6,7,8,9].map(x=> x*10);
 console.log(fiveTimes);
 
+//sum of array
+
+function sumArray(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+
+const myArray = [1, 2, 3, 4, 5];
+const total = sumArray(myArray);
+console.log(total); // Output: 15
+
+
+
+
