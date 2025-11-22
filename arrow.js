dbf3169fc69bcdb2fbc599169301a702cdb325e1
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2025-11-22 17:29:50 +0600

    Add examples of traditional and arrow functions
    Added examples of using traditional and arrow functions with map to double numbers.

--- arrow.js
+++ arrow.js
@@ -1,3 +1,18 @@
+const numbers = [1, 2, 3, 4];
+
+// Using a traditional function with map
+const doubledNumbersTraditional = numbers.map(function(number) {
+  return number * 2;
+});
+
+// Using an arrow function with map (implicit return)
+const doubledNumbersArrow = numbers.map(number => number * 2);
+
+console.log(doubledNumbersTraditional); // Output: [2, 4, 6, 8]
+console.log(doubledNumbersArrow);    // Output: [2, 4, 6, 8]
+
+
+
 // function declaration 
 function add (first ,second){
     const total = first + second ;
@@ -13,5 +28,6 @@ const add3 = (first, second) => first+second;
 
 const result =  add3 (10, 20);
 console.log(result);
+
 
 
