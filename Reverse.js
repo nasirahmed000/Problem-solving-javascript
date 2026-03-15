872fe01931e542f8367faefaf511faab6c33b7be
Nasir Ahmed <80189624+nasirahmed000@users.noreply.github.com>
2026-03-16 00:13:01 +0600

    Create Reverse.js

---
+++ Reverse.js
@@ -0,0 +1,17 @@
+
+
+function reverseString(str){
+    return str.split('').reverse().join('');
+}
+
+console.log(reverseString("hello"));
+
+// Example
+
+// Input:
+
+// "hello"
+
+// Output:
+
+// "olleh"
